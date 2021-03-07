4b02884692ccb6f5a00fc13a301c48e330eaf054
Maxim Kalevich <maxkalevich@gmail.com>
2021-03-08 00:41:06 +0300

    init: complete 11/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let a = 0;
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+
+            if (str[i] === bracketsConfig[j][i]) {
+                stack.push(str[i])
+            }
+            else {
+                a++
+            }
+        }
+    }
+    if (a !== 0) return false
+    if (a === 0) return true
 }
