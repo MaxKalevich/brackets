5ad5b26cf667e837a7c8ba128cb3ea72a90b5081
Maxim Kalevich <maxkalevich@gmail.com>
2021-03-13 00:02:52 +0300

    init: complete 20/20

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 
 module.exports = function check(str, bracketsConfig) {
-    let stack = [];
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === "(" || str[i] === "[" || str[i] === "|") {
-            stack.push(str[i])
-
-        }
-        if ((str[i] === ')' && stack[stack.length - 1] === "(") || (str[i] === ']' && stack[stack.length - 1] === "[") || (str[i] === '|' && stack[stack.length - 1] === "|")) {
-            stack.pop()
-        }
+    const exampleBrackets = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        const bracket = bracketsConfig[i].join("");
+        exampleBrackets.push(bracket)
     }
-    if (stack.length === 0) return true;
-    else return false;
+    for (let i = 0; i < 100; i++) {
+        exampleBrackets.forEach(el => {
+            if (str.includes(el)) {
+                let strNew = str.replace(el, "")
+                str = strNew
+            }
+        })
+    }
+
+    if (str.length === 0) return true
+    if (str.length !== 0) return false
 }
